ab17a08c9ca4f6fba4d67be9b09a6ca9392d7fec
Bruno Guerra <g.g.bruno.00@gmail.com>
2021-03-31 22:57:44 -0300

    Adding uppercase to both currencies.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         eprintln!("Missing ARGS: base_currency [USD] wanted_currency [BRL]");
         std::process::exit(1)
     }
-    let base_currency = &args[1];
+    let base_currency = &args[1].to_uppercase();
     let wanted_currency = &args[2].to_uppercase();
     let url = "https://api.exchangeratesapi.io/latest?base=".to_owned() + base_currency;
     let res: ResponseType = reqwest::get(url)
